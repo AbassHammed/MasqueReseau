e5c25636c3befaf4713cbf170e244d1974a9f871
Hammed <abasshammedola@gmail.com>
2024-05-16 00:49:05 +0200

    Improving the condition fo enable the button

--- frmHome.cs
+++ frmHome.cs
@@ -1,3 +1,4 @@
+using Reseau.lib;
 namespace Reseau
 {
     public partial class frmHome : Form
@@ -114,55 +115,129 @@ namespace Reseau
 
         private void CheckFields()
         {
-            if (FilledAndInt())
-            {
-                lblMsg.ForeColor = Color.Green;
-                lblMsg.Text = "Une condition permettant de faire les calculs est remplie";
-                btnValider.Enabled = true;
-            }
-            else
+            if (!verifyIp())
             {
                 lblMsg.ForeColor = Color.Red;
                 lblMsg.Text = "Veuillez remplir les champs correctement";
                 btnValider.Enabled = false;
             }
+            else if (!verifyMsq())
+            {
+                lblMsg.ForeColor = Color.Red;
+                lblMsg.Text = "Veuillez respecter les limites";
+                btnValider.Enabled = false;
+            }
+            else
+            {
+                lblMsg.ForeColor = Color.Green;
+                lblMsg.Text = "Tous les champs sont correctement remplis";
+                btnValider.Enabled = true;
+            }
 
 
         }
 
-        private bool FilledAndInt()
+        private void setValuesIp(bool DecEnable = true, bool BiEnable = true, bool HexEnable = true)
         {
-            bool ipFilled = ChampsRemplisEtEntiers(txtDEC1, txtDEC2, txtDEC3, txtDEC4) ||
-                            ChampsRemplisEtEntiers(txtBI1, txtBI2, txtBI3, txtBI4) ||
-                            ChampsRemplis(txtHEX1, txtHEX2, txtHEX3, txtHEX4);
-            bool msgFilled = ChampsRemplisEtEntiers(txtMsqDEC1, txtMsqDEC2, txtMsqDEC3, txtMsqDEC4) ||
-                             ChampsRemplisEtEntiers(txtMsqBI1, txtMsqBI2, txtMsqBI3, txtMsqBI4) ||
-                             int.TryParse(txtMsqCIDR.Text, out _);
-            return ipFilled && msgFilled;
+            txtDEC1.Enabled = DecEnable;
+            txtDEC2.Enabled = DecEnable;
+            txtDEC3.Enabled = DecEnable;
+            txtDEC4.Enabled = DecEnable;
+            txtBI1.Enabled = BiEnable;
+            txtBI2.Enabled = BiEnable;
+            txtBI3.Enabled = BiEnable;
+            txtBI4.Enabled = BiEnable;
+            txtHEX1.Enabled = HexEnable;
+            txtHEX2.Enabled = HexEnable;
+            txtHEX3.Enabled = HexEnable;
+            txtHEX4.Enabled = HexEnable;
         }
 
-        private bool ChampsRemplisEtEntiers(params TextBox[] champs)
+        private void setValuesMsq(bool DecEnable = true, bool BiEnable = true, bool EnableCidr = true)
         {
-            foreach (var champ in champs)
-                if (string.IsNullOrWhiteSpace(champ.Text) || !int.TryParse(champ.Text, out _))
-                    return false;
-            return true;
+            txtMsqDEC1.Enabled = DecEnable;
+            txtMsqDEC2.Enabled = DecEnable;
+            txtMsqDEC3.Enabled = DecEnable;
+            txtMsqDEC4.Enabled = DecEnable;
+            txtMsqBI1.Enabled = BiEnable;
+            txtMsqBI2.Enabled = BiEnable;
+            txtMsqBI3.Enabled = BiEnable;
+            txtMsqBI4.Enabled = BiEnable;
+            txtMsqCIDR.Enabled = EnableCidr;
         }
 
-        private bool ChampsRemplis(params TextBox[] champs)
+
+        private bool verifyIpDec()
         {
-            foreach (var champ in champs)
-                if (string.IsNullOrWhiteSpace(champ.Text))
-                    return false;
-            return true;
+            if (Utils.ChampsRemplis(txtDEC1) || Utils.ChampsRemplis(txtDEC2) || Utils.ChampsRemplis(txtDEC3) || Utils.ChampsRemplis(txtDEC4))
+                setValuesIp(DecEnable: true, BiEnable: false, HexEnable: false);
+            else
+                setValuesIp();
+
+
+            return Utils.ChampsEntiers(txtDEC1, txtDEC2, txtDEC3, txtDEC4) && Utils.ChampsRemplis(txtDEC1, txtDEC2, txtDEC3, txtDEC4) && Utils.ChampsDansLaLimite(255, txtDEC1, txtDEC2, txtDEC3, txtDEC4);
         }
 
-        private bool ChampsDansLaLimite(int limite, params TextBox[] champs)
+
+        private bool verifyIpBi()
         {
-            foreach (var champ in champs)
-                if (!int.TryParse(champ.Text, out int valeur) || valeur < 0 || valeur > limite)
-                    return false;
-            return true;
+            if (Utils.ChampsRemplis(txtBI1) || Utils.ChampsRemplis(txtBI2) || Utils.ChampsRemplis(txtBI3) || Utils.ChampsRemplis(txtBI4))
+                setValuesIp(BiEnable: true, DecEnable: false, HexEnable: false);
+            else
+                setValuesIp();
+
+            return Utils.ChampsBinaires(txtBI1, txtBI2, txtBI3, txtBI4) && Utils.ChampsRemplis(txtBI1, txtBI2, txtBI3, txtBI4);
+        }
+
+        private bool verifyIpHex()
+        {
+            if (Utils.ChampsRemplis(txtHEX1) || Utils.ChampsRemplis(txtHEX2) || Utils.ChampsRemplis(txtHEX3) || Utils.ChampsRemplis(txtHEX4))
+                setValuesIp(HexEnable: true, DecEnable: false, BiEnable: false);
+            else
+                setValuesIp();
+
+            return Utils.ChampsHexadecimaux(txtHEX1, txtHEX2, txtHEX3, txtHEX4) && Utils.ChampsRemplis(txtHEX1, txtHEX2, txtHEX3, txtHEX4);
+        }
+
+
+        private bool verifyIp()
+        {
+            return verifyIpDec() || verifyIpBi() || verifyIpHex();
+        }
+
+        private bool verifyMsqDec()
+        {
+            if (Utils.ChampsRemplis(txtMsqDEC1) || Utils.ChampsRemplis(txtMsqDEC2) || Utils.ChampsRemplis(txtMsqDEC3) || Utils.ChampsRemplis(txtMsqDEC4))
+                setValuesMsq(DecEnable: true, BiEnable: false, EnableCidr: false);
+            else
+                setValuesMsq();
+
+            return Utils.ChampsEntiers(txtMsqDEC1, txtMsqDEC2, txtMsqDEC3, txtMsqDEC4) && Utils.ChampsRemplis(txtMsqDEC1, txtMsqDEC2, txtMsqDEC3, txtMsqDEC4) && Utils.ChampsDansLaLimite(255, txtMsqDEC1, txtMsqDEC2, txtMsqDEC3, txtMsqDEC4);
+        }
+
+        private bool verifyMsqBi()
+        {
+            if (Utils.ChampsRemplis(txtMsqBI1) || Utils.ChampsRemplis(txtMsqBI2) || Utils.ChampsRemplis(txtMsqBI3) || Utils.ChampsRemplis(txtMsqBI4))
+                setValuesMsq(BiEnable: true, DecEnable: false, EnableCidr: false);
+            else
+                setValuesMsq();
+
+            return Utils.ChampsBinaires(txtMsqBI1, txtMsqBI2, txtMsqBI3, txtMsqBI4) && Utils.ChampsRemplis(txtMsqBI1, txtMsqBI2, txtMsqBI3, txtMsqBI4);
+        }
+
+        private bool verifyMsqCidr()
+        {
+            if (Utils.ChampsRemplis(txtMsqCIDR))
+                setValuesMsq(EnableCidr: true, BiEnable: false, DecEnable: false);
+            else
+                setValuesMsq();
+            return Utils.ChampsEntiers(txtMsqCIDR) && Utils.ChampsRemplis(txtMsqCIDR) && Utils.ChampsDansLaLimite(32, txtMsqCIDR);
+        }
+
+
+        private bool verifyMsq()
+        {
+            return verifyMsqDec() || verifyMsqBi() || verifyMsqCidr();
         }
 
 
---
+++ lib/utils.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reseau.lib
+{
+    public class Utils
+    {
+
+        public static bool ChampsEntiers(params TextBox[] champs)
+        {
+            foreach (var champ in champs)
+                if (!int.TryParse(champ.Text, out _))
+                    return false;
+            return true;
+        }
+
+        public static bool ChampsRemplisEtEntiers(params TextBox[] champs)
+        {
+            return ChampsRemplis(champs) && ChampsEntiers(champs);
+        }
+
+        public static bool ChampsRemplis(params TextBox[] champs)
+        {
+            foreach (var champ in champs)
+                if (string.IsNullOrWhiteSpace(champ.Text))
+                    return false;
+            return true;
+        }
+
+        public static bool ChampsDansLaLimite(int limite, params TextBox[] champs)
+        {
+            foreach (var champ in champs)
+                if (!int.TryParse(champ.Text, out int valeur) || valeur < 0 || valeur > limite)
+                    return false;
+            return true;
+        }
+
+        private static bool EstBinaire(string text)
+        {
+            return text.All(c => c == '0' || c == '1');
+        }
+
+
+        public static bool ChampsBinaires(params TextBox[] champs)
+        {
+            foreach (var champ in champs)
+                if (!EstBinaire(champ.Text))
+                    return false;
+            return true;
+        }
+
+        public static bool ChampsHexadecimaux(params TextBox[] champs)
+        {
+            foreach (var champ in champs)
+                if (!int.TryParse(champ.Text, System.Globalization.NumberStyles.HexNumber, System.Globalization.CultureInfo.InvariantCulture, out _))
+                    return false;
+            return true;
+        }
+
+
+    }
+}
